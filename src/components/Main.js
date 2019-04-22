380f7b99a38d90b36b5b17ac0030c410cafa72bf
Madeline Rombes <madelinerombes@gmail.com>
2019-04-22 12:40:18 -0400

    rendered laptop at top

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import bridge from '../images/bridge.jpg'
-import me from '../images/me.jpg'
+import laptop from '../images/laptop.png'
 import Header from './Header'
 
 const Main = () => {
     return (
       <div>
-        <img className="main_background" alt="bridge" src={bridge} />
+        <img className="main_background" alt="bridge" src={laptop} />
         <Header />
-        <img className="me_img" alt="me" src={me} />
       </div>
     )
 }
