7ab0bae5337c4bacd2f04dee4d7e231a5c8a3846
Madeline Rombes <madelinerombes@gmail.com>
2019-04-22 12:54:13 -0400

    resized logos

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import me from '../images/me.jpg'
 import css from '../images/logos/css.png'
 import git from '../images/logos/git.png'
 import github from '../images/logos/github.png'
@@ -20,22 +21,23 @@ const About = () => {
         <div className="about" id="about">
             <h3 className="about_header">ABOUT</h3>
             <p className="about_description">Full stack web developer with a passion for beautiful, responsive products.<br/>Always learning.</p>
+            <img className="me_img" src={me} />
             <div className="about_logos">
-                <img className="about_logos-img"  src={javascript} />
-                <img className="about_logos-img"  src={react} />
-                <img className="about_logos-img" src={css} />
-                <img className="about_logos-img"  src={html} />
-                <img className="about_logos-img"  src={git} />
-                <img className="about_logos-img"  src={github} />
-                <img className="about_logos-img"  src={heroku} />
-                <img className="about_logos-img"  src={postgresql} />
-                <img className="about_logos-img"  src={sequelize} />
-                <img className="about_logos-img"  src={rails} />
-                <img className="about_logos-img"  src={ruby} />
-                <img className="about_logos-img"  src={zeplin} />
-                <img className="about_logos-img"  src={photoshop} />
-                <img className="about_logos-img"  src={illustrator} />
-                <img className="about_logos-img"  src={indesign} />
+                <img className="about_logos-img" src={javascript} />
+                <img className="about_logos-img css" src={css} />
+                <img className="about_logos-img html" src={html} />
+                <img className="about_logos-img" src={react} />
+                <img className="about_logos-img" src={git} />
+                <img className="about_logos-img github" src={github} />
+                <img className="about_logos-img heroku" src={heroku} />
+                <img className="about_logos-img" src={postgresql} />
+                <img className="about_logos-img" src={sequelize} />
+                <img className="about_logos-img" src={rails} />
+                <img className="about_logos-img ruby" src={ruby} />
+                <img className="about_logos-img" src={zeplin} />
+                <img className="about_logos-img adobe" src={photoshop} />
+                <img className="about_logos-img adobe" src={illustrator} />
+                <img className="about_logos-img adobe" src={indesign} />
             </div>
         </div>
     )
