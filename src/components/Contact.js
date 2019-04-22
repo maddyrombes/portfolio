5ea1f0dc1103c8b553ea67d7fa8de5a292997e90
Madeline Rombes <madelinerombes@gmail.com>
2019-04-22 13:13:29 -0400

    pic at bottom

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import laptopContact from '../images/laptop-contact.JPG'
 
 const Contact = () => {
     return (      
@@ -6,6 +7,7 @@ const Contact = () => {
             <h3 className="contact_header">CONTACT</h3>
             <a className="contact_link" href="mailto:madelinerombes@gmail.com" target="blank">EMAIL ME</a>
             <a className="contact_link" href="https://www.linkedin.com/in/maddyrombes/" target="blank">LINKEDIN</a>
+            <img className="contact_laptop" alt="laptop" src={laptopContact} />
         </div>
     )
 }
