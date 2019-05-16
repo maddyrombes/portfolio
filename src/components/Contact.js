95f8455fb7a22b721446d95f1cab4120c4b99184
Madeline Rombes <madelinerombes@gmail.com>
2019-05-16 17:03:58 -0400

    styling form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,18 +5,12 @@ const Contact = () => {
         <div className="contact" id="contact">
             <h3 className="contact_header">CONTACT</h3>
 
-            <form>
-                <label>Name</label>
-                <input type="text"/>
-                <label>Email</label>
-                <input type="text"/>
-                <label>Message</label>
-                <input type="text"/>
-                <button>Submit</button>
+            <form className="contact-form">
+                <input className="name-input" type="text" placeholder="Name"/>
+                <input className="email-input" type="text" placeholder="Email"/>
+                <input className="message-input" type="text" placeholder="Your message" />
+                <button className="contact-submit">Submit</button>
             </form>
-
-            <a className="contact_link" href="mailto:madelinerombes@gmail.com" target="blank">EMAIL ME</a>
-            <a className="contact_link" href="https://www.linkedin.com/in/maddyrombes/" target="blank">LINKEDIN</a>
         </div>
     )
 }
