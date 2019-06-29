ad9f645f020460424f41db0d49bdb6d8ff8be170
Madeline Rombes <madelinerombes@gmail.com>
2019-06-29 15:26:28 -0400

    fix contact image

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import butterfly from '../images/butterfly.JPG'
 
 
 const Contact = () => {
-    return (      
-        <div className="contact" id="contact">
-            <h3 className="contact_header">CONTACT</h3>
+  return (
+    <div className="contact" id="contact">
+      <h3 className="contact_header">CONTACT</h3>
 
-            <img className="butterfly" src={butterfly} />
+      {/* <img className="butterfly" src={butterfly} /> */}
 
-            <form className="contact-form">
-                <input className="name-input" type="text" placeholder="Name"/>
-                <input className="email-input" type="text" placeholder="Email"/>
-                <input className="message-input" type="text" placeholder="Your message" />
-                <button className="contact-submit">Submit</button>
-            </form>
-        </div>
-    )
+      <form className="contact-form">
+        <input className="name-input" type="text" name="name" placeholder="Name" required />
+        <input className="email-input" type="text" name="email" placeholder="Email" required />
+        <input className="message-input" type="text" name="message" placeholder="Your message" required />
+        <input type="submit" className="contact-submit" value="Submit" />
+      </form>
+    </div>
+  )
 }
 
 export default Contact
