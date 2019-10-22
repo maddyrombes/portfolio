6149efb7a35bb11adc3b0faff93441ff5b80e9ae
Madeline Rombes <madelinerombes@gmail.com>
2019-10-22 19:04:52 -0400

    transform logos

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ const Logo = props => {
   const { alt, className, src, title } = props;
 
   return (
-    <div>
+    <div className="logo">
       <img alt={alt} className={className} src={src} />
       <p className="logo-title">{title}</p>
     </div>
